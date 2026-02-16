1e9945319faa9713524f4f22f9c82a616529c9fc
Brian Matheson <bmath@brianmatheson.com>
2026-02-15 23:24:46 -0500

    ebs

--- deploy/k8s/requirements.sh
+++ deploy/k8s/requirements.sh
@@ -7,3 +7,23 @@ helm upgrade --install cnpg \
   cnpg/cloudnative-pg
 
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.19.2/cert-manager.yaml
+
+kubectl apply -f - <<EOF
+EOF
+apiVersion: storage.k8s.io/v1
+kind: StorageClass
+metadata:
+  name: standard
+  annotations:
+    storageclass.kubernetes.io/is-default-class: "true"
+allowedTopologies:
+- matchLabelExpressions:
+  - key: eks.amazonaws.com/compute-type
+    values:
+    - auto
+provisioner: ebs.csi.eks.amazonaws.com
+volumeBindingMode: WaitForFirstConsumer
+parameters:
+  type: gp3
+  encrypted: "true"
+EOF
